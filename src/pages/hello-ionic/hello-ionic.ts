3e4c5e9ae58931b29dde44173d7761f017b8ad8c
Xinke Yi <andy.yiwhu@gmail.com>
2017-09-25 20:13:04 -0500

    add a little bit on second page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {RestaurantPage} from '../restaurant/restaurant';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  
+  constructor(public nav: NavController) {
 
+  }
+
+  showDetails(){
+     this.nav.push(RestaurantPage,{
+       name: 'Joy Yee Noodle',
+       Rating: '70%'
+     });
   }
 }
