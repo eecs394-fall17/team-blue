8a2c4e0ec8bc7fceb0e63b4b835cd98da946479c
LJGladic <lukasgladic2017@u.northwestern.edu>
2017-10-09 17:28:01 -0500

    Change the firebase intialization
    I put it in the right place this time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,15 +27,6 @@ export class MyApp {
     public statusBar: StatusBar,
     public splashScreen: SplashScreen
 
-    firebase.initializeApp({
-      apiKey: "AIzaSyB9OHQ2wJ1nQiHo18RHve0fyTZ4Qi8F5Qc",
-      authDomain: "shacuisine-a43ab.firebaseapp.com",
-      databaseURL: "https://shacuisine-a43ab.firebaseio.com",
-      projectId: "shacuisine-a43ab",
-      storageBucket: "shacuisine-a43ab.appspot.com",
-      messagingSenderId: "753679163545"
-    })
-
 
   ) {
     this.initializeApp();
@@ -45,6 +36,15 @@ export class MyApp {
       { title: 'Hello Ionic', component: HelloIonicPage },
       { title: 'My First List', component: ListPage }
     ];
+
+     firebase.initializeApp({
+      apiKey: "AIzaSyB9OHQ2wJ1nQiHo18RHve0fyTZ4Qi8F5Qc",
+      authDomain: "shacuisine-a43ab.firebaseapp.com",
+      databaseURL: "https://shacuisine-a43ab.firebaseio.com",
+      projectId: "shacuisine-a43ab",
+      storageBucket: "shacuisine-a43ab.appspot.com",
+      messagingSenderId: "753679163545"
+    })
   }
 
   initializeApp() {
