8db1fd434a6adcdb01a854011a622b872639ceb7
LJGladic <lukasgladic2017@u.northwestern.edu>
2017-10-12 18:04:18 -0500

    more syntax errors

--- src/pages/restaurant-service.ts
+++ src/pages/restaurant-service.ts
@@ -33,7 +33,7 @@ export class RestaurantService {
 restaurants: Array<RestaurantItem>;
 joyyeename: string;
 joyyeetag: string;
-joyyyerating: string;
+joyyeerating: string;
 joyyeedescription: string;
 
 
