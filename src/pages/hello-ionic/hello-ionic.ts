12b6fa4e9b9216813202417a52e981b1ba9f6629
LJGladic <lukasgladic2017@u.northwestern.edu>
2017-10-23 23:54:29 -0500

    Just work

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -34,10 +34,11 @@ export class HelloIonicPage {
     this.restaurants = db.list("Restaurants");
     //this.restaurants.subscribe(restaurants => this.restaurants_as_array = this.restaurants);
     this.restaurants_as_array = this.restaurants;
-    this.restaurants_as_array = Object.keys(this.restaurants_as_array).map((key) => {
+    /**this.restaurants_as_array = Object.keys(this.restaurants_as_array).map((key) => {
 
           return this.restaurants_as_array[key];
-    } );
+    } ); 
+    **/
 
 
 /**
